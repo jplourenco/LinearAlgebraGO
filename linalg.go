c7c423734f0b984912c9dacf97db6a8e87a1ddb5
mindsculptor <jp.lourenco@campus.fct.unl.pt>
2014-12-17 01:47:15 +0000

    Update 2
    Added Scalar Multiplication, Matrix Addition, Subtraction, element
    DIvision, Multiplication

--- linalg.go
+++ linalg.go
@@ -11,9 +11,7 @@ func eye(x int) Matrix{
 	for i := 0 ; i < x ;i++ {
 		mat[i] = make([]int,x)
 		mat[i][i] = 1
-		
 	}
-	
 	return mat
 }
 
@@ -24,11 +22,11 @@ func dot(a,b []int ) int{
 	for i := 0; i < l; i++{
 		res += a[i]*b[i]
 	}
-
 	return res
 }
 
 func transpose(a Matrix) Matrix{
+	
 	mat := make([][]int,len(a[0]))
 
 	for i:= 0 ; i < len(a[0]); i++{
@@ -37,7 +35,6 @@ func transpose(a Matrix) Matrix{
 			mat[i][j] = a[j][i]
 		}
 	}
-
 	return mat
 }
 
@@ -57,8 +54,92 @@ func mult(a,b Matrix) Matrix{
 			res[i][j] = dot(a[i],tb[j])
 		}
 	}
-
 	return res
+}
+
+func scalarMult(a Matrix, n int) Matrix{
+
+	mat := make([][]int,len(a))
+
+	for i := 0; i < len(a); i++ {
+		mat[i] = make([]int,len(a[0]))
+		if n != 0 {
+			for j := 0; j < len(a[0]);j++{
+				mat[i][j] = n*a[i][j]
+			}
+		}
+	}
+	return mat
+}
+
+func add(a,b Matrix) Matrix{
+
+	if len(a) != len(b) || len(a[0]) != len(b[0]) {
+		return nil
+	}
+
+	mat := make([][]int,len(a))
+
+	for i := 0; i < len(a); i++ {
+		mat[i] = make([]int,len(a[0]))
+		for j := 0; j < len(a[0]); j++{
+			mat[i][j] = a[i][j] + b[i][j]
+		}
+	}
+
+	return mat
+}
+
+func sub(a,b Matrix) Matrix{
+
+	if len(a) != len(b) || len(a[0]) != len(b[0]) {
+		return nil
+	}
+
+	mat := make([][]int,len(a))
+
+	for i := 0; i < len(a); i++ {
+		mat[i] = make([]int,len(a[0]))
+		for j := 0; j < len(a[0]); j++{
+			mat[i][j] = a[i][j] - b[i][j]
+		}
+	}
+
+	return mat
+}
+
+func eMult(a,b Matrix) Matrix{
+	if len(a) != len(b) || len(a[0]) != len(b[0]) {
+		return nil
+	}
+
+	mat := make([][]int,len(a))
+
+	for i := 0; i < len(a); i++ {
+		mat[i] = make([]int,len(a[0]))
+		for j := 0; j < len(a[0]); j++{
+			mat[i][j] = a[i][j] * b[i][j]
+		}
+	}
+
+	return mat
+}
+
+func eDiv(a,b Matrix) Matrix{
+	
+	if len(a) != len(b) || len(a[0]) != len(b[0]) {
+		return nil
+	}
+
+	mat := make([][]int,len(a))
+
+	for i := 0; i < len(a); i++ {
+		mat[i] = make([]int,len(a[0]))
+		for j := 0; j < len(a[0]); j++{
+			mat[i][j] = a[i][j] / b[i][j]
+		}
+	}
+	return mat
 }
 
 func main() {
@@ -70,8 +151,13 @@ func main() {
 
 	z := [][]int{a,b}
 	y := [][]int{c,c}
+
 	//fmt.Println(z)
-	fmt.Println(eye(4))
-	fmt.Println(mult(z,y))
+	fmt.Println(scalarMult(eye(4),10))
+	fmt.Println(eDiv(z,y))
+	fmt.Println(eDiv(z,z))
+
+
+
 }
 
